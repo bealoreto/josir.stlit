e1c39b62d577ca16b86b83934219633871db4bb7
bealoreto <loretobia@gmail.com>
2025-05-23 10:59:08 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -9,3 +9,11 @@ Hello *world*
 nome = st.text_input('Digite o seu nome: ')
 if len(nome) > 0:
   st.write ('Que nome maneiro!')
+
+idade = st.text_input('Digite a sua idade: ')
+if len(idade) > 0:
+  st.write ('Na flor da idade...')
+
+altura = st.text_input('Digite a sua altura: ')
+if len(altura) > 0:
+  st.write ('A altura perfeita :)')

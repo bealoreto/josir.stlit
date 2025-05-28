df39b8ae95245e4c454aa2391727a63d05eac699
bealoreto <loretobia@gmail.com>
2025-05-28 12:40:05 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,19 +1,30 @@
-import streamlit as st
 import pandas as pd
+import streamlit as st
+import requests 
+import json
 
-st.write("""
-# My first app
-Hello *world*
+st.write ("""
+# Oportunidades para dançarinos 
+Você vive da dança e quer encontrar oportunidade de trabalho? Aqui é *seu lugar*!
 """)
+cidade = st.text_input('Digite o código da cidade: ')
+if len(cidade) > 0:
+  st.write ('Ok!')
+data = st.text_input('Digite o período de interesse: ')
+if len(data) > 0:
+    st.write ('Ok!')
 
-nome = st.text_input('Digite o seu nome: ')
-if len(nome) > 0:
-  st.write ('Que nome maneiro!')
 
-idade = st.text_input('Digite a sua idade: ')
-if len(idade) > 0:
-  st.write ('Na flor da idade...')
+url= 'https://queridodiario.ok.org.br/api/gazettes?territory_ids=3304557&published_since=2025-01-01&querystring=dan%C3%A7a&excerpt_size=1000&number_of_excerpts=1&pre_tags=&post_tags=&size=10&sort_by=relevance'
+response = requests.get(url)
 
-altura = st.text_input('Digite a sua altura: ')
-if len(altura) > 0:
-  st.write ('A altura perfeita :)')
+st.subheader(f'Foram encontrados {response.json()["total_gazettes"]} entradas no diário oficial')
+
+st.write(response.json())
+
+
+for resultado in response.json()['gazettes'][:10]:
+    st.write("Data:", resultado["date"])
+    for excerpt in resultado['excerpts']:
+        st.write(excerpt)
+        st.write('---')
